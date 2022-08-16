9ab8e5660f8413b0eb68e452865a8d08d71ffc58
Aleksey <lexus1988@bk.ru>
2022-08-16 13:21:43 +0300

    задача 0, квадрат числа

---
+++ Seminar 1/test/Program.cs
@@ -0,0 +1,52 @@
+﻿// создадим рандомный массив чисел N 
+
+
+
+
+Console.WriteLine("Введите размер массива: ");
+int N  = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N]; //создает массив
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 999);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+int count = col.Length;
+int position = 0;
+while (position < count)
+{
+    Console.Write(col[position] + " ");
+    position++;
+}
+}
+
+
+
+
+FillArray(array);
+Console.WriteLine("Случайный массив:");
+PrintArray(array);
+
+int maxval = array.Max();
+Console.WriteLine("");
+Console.WriteLine("Максимальный элемент масиива: " + maxval);
+
+int indexMax = Array.FindIndex(array, item => item == maxval);
+Console.WriteLine("Индекс максимального элемента массива: " + indexMax);
+
+int minval = array.Min();
+Console.WriteLine("Минимальный элемент масиива: " + minval);
+
+int indexMin = Array.FindIndex(array, item => item == minval);
+Console.WriteLine("Индекс минимального элемента массива: " + indexMin);
---
+++ Seminar 2/test/Program.cs
@@ -0,0 +1,47 @@
+﻿// // создание метода 
+
+// Console.WriteLine("Введите число для вычисления факториала: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+// void fac(int a)
+// {
+
+//     if (N == 0)
+
+//     {
+//         Console.WriteLine("!0 = 1");
+//     }
+//     else
+//     {
+//         int F = 1;
+
+//         for (int i = 1; i <= N; i++)
+//         {
+//             F = F * i;
+//         }
+
+//         Console.WriteLine($"!{N}={F}");
+//     }
+// }
+
+// fac(N);
+
+double x = Math.E;
+Console.WriteLine("E={0:##.##}", x); // не более шести знаков
+Console.WriteLine("E={0:0.00}", x); // ровно шесть знаков
+Console.WriteLine("E={0:F2}", x); // число с плавающей точкой и 6 знаков после запятой
+
+Console.WriteLine("N");
+
+double N = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("M");
+
+double M = Convert.ToInt32(Console.ReadLine());
+double d = N/M;
+Console.WriteLine("d={0:##.##}", d);
