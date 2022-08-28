59190cf3520595fc3b4d0f11dba9650e00360e20
Aleksey <lexus1988@bk.ru>
2022-08-28 13:23:08 +0300

    Вычисление элемента m+n

---
+++ Seminar 7/Task48/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.WriteLine("Введите размерность матрицы через пробел:");
+string s = Console.ReadLine();
+string[] s_arr = s.Split();
+int M = int.Parse(s_arr[0]);
+int N = int.Parse(s_arr[1]);
+
+int[,] matrix = new int [M,N];
+
+void FillArray2D(int[,] matr)
+{
+
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j =0; j < matr.GetLength(1); j++)
+    {
+        matr[i,j] = i+j;
+    }
+}
+
+}
+
+void PrintArray2D(int[,] array2d)
+{
+for (int i = 0; i < array2d.GetLength(0); i++ )
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        Console.Write($"{array2d[i,j]}"+" ");
+        Console.WriteLine();
+    }
+    
+}
+
+FillArray2D(matrix);
+PrintArray2D(matrix);
