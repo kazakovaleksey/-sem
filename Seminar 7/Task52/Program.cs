0ed60c4b9c537e89a205023c302763a618c75d1b
Aleksey <lexus1988@bk.ru>
2022-08-28 16:57:06 +0300

    среднее арифметическое в столбце_вторая попытка

--- Seminar 7/Task52/Program.cs
+++ Seminar 7/Task52/Program.cs
@@ -13,50 +13,65 @@ string[] s_arr = s.Split();
 int M = int.Parse(s_arr[0]);
 int N = int.Parse(s_arr[1]);
 
-int[,] matrix = new int [M,N];
-double mid = new double [N];
+int[,] matrix = new int[M, N];
+double[] mid = new double[N];
 
 void PrintArray2D(int[,] array2d)
 {
-for (int i = 0; i < array2d.GetLength(0); i++ )
+    for (int i = 0; i < array2d.GetLength(0); i++)
     {
         for (int j = 0; j < array2d.GetLength(1); j++)
-        Console.Write($"{array2d[i,j]}"+" ");
+            Console.Write(array2d[i, j] + " ");
         Console.WriteLine();
     }
-    
+
+}
+
+void PrintArray1D(double[] array1d)
+{
+    for (int i = 0; i < array1d.Length; i++)
+    {
+
+        Console.Write(Math.Round(array1d[i], 2) + "; ");
+        
+    }
+
 }
 
 
 void FillArray2D(int[,] matr)
 {
 
-for (int i = 0; i < matr.GetLength(0); i++)
-{
-    for (int j =0; j < matr.GetLength(1); j++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        matr[i,j] = new Random().Next(1,10);
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
     }
-}
 
 }
 
 
 void MidColumns(int[,] sourcearray, double[] midarray)
 {
-    double mid =0;
-    for (int j=0; j < sourcearray.GetLength(1); j++)
-        for (int i =0; i < sourcearray.GetLength(0); i++)
-        {
-        mid = mid + sourcearray[i,j];
-        }
-        midarray[j] = mid/sourcearray.GetLength(0);
+    double mid = 0;
+    for (int j = 0; j < sourcearray.GetLength(1); j++)
+    {
         mid = 0;
+        for (int i = 0; i < sourcearray.GetLength(0); i++)
+        {
+            mid = mid + sourcearray[i, j];
+        }
+        midarray[j] = mid / sourcearray.GetLength(0);
+
+    }
 }
 
 FillArray2D(matrix);
 Console.WriteLine($"Массив случайных чисел {M}x{N}:");
 PrintArray2D(matrix);
+MidColumns(matrix, mid);
 Console.WriteLine(string.Empty);
-Console.WriteLine(string.Join(" ", midarray));
-
+Console.WriteLine("Искомое среднее арифметичекое по столбцам приведно в массиве:");
+PrintArray1D(mid);
