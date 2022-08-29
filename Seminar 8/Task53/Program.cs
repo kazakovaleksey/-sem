b4f4c09c49d092ccf4deac0a3f78583cb59acb4b
Aleksey <lexus1988@bk.ru>
2022-08-29 20:49:51 +0300

    замена первой строки на последнюю

---
+++ Seminar 8/Task53/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+
+Console.WriteLine("Введите размерность матрицы через пробел:");
+string s = Console.ReadLine();
+string[] s_arr = s.Split();
+int M = int.Parse(s_arr[0]);
+int N = int.Parse(s_arr[1]);
+
+int[,] matrix = new int[M, N];
+void PrintArray2D(int[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+            Console.Write(array2d[i, j] + " ");
+        Console.WriteLine();
+    }
+
+}
+
+void FillArray2D(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+
+}
+
+
+void ReverseArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[0,j];
+        array[0,j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+}
+
+
+FillArray2D(matrix);
+Console.WriteLine($"Массив случайных чисел {M}x{N}:");
+PrintArray2D(matrix);
+
+ReverseArray(matrix);
+Console.WriteLine("поменяны местами 1 и последние строки:");
+PrintArray2D(matrix);
+
