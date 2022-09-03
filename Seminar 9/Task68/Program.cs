177eda8ee52cd6e9530f148a46b0da5701c2a799
Aleksey <lexus1988@bk.ru>
2022-09-03 12:45:03 +0300

    Натуральные числа от N до 1

---
+++ Seminar 9/Task68/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите размерность массива через пробел :");
+string s = Console.ReadLine();
+string[] s_arr = s.Split();
+int N = int.Parse(s_arr[0]);
+int M = int.Parse(s_arr[1]);
+
+if (M >= 0 && N >= 0)
+{
+    int sum = Sum(N, M);
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Числа не могут быть отрицательными!!!");
+}
+
+int Sum(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Sum(m - 1, 1);
+    }
+    else
+    {
+        return Sum(m - 1, Sum(m, n - 1));
+    }
+}
